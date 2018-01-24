792b1f8ece9719df8ac77073bb9c28a19934ec8d
Peter Pham <31864835+pizzapop2457@users.noreply.github.com>
2018-01-24 13:13:19 -0700

    Add files via upload
    finished with Christian and Matt

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,9 @@
-
-
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string songs[]= {"Flash","Arashi no Naka de Kagayaite", "Uptown Funk","Fireflies","Like OOH-AHH","Something Just Like This","My Way","Pumped up Kicks"};
+	for (int i = 0; i < 8; i++)
+		cout << songs[i] << endl;
+	return 0;
+}
